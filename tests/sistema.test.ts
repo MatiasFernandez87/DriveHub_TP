4c6c48e99856edc02800eb944213fc980d9126c7
Matias Fernandez <dev.mattt@gmail.com>
2025-11-15 14:16:29 -0300

    Actualizacion Sistema

--- tests/sistema.test.ts
+++ tests/sistema.test.ts
@@ -1,43 +1,69 @@
 import Cliente from "../src/cliente";
 import Vehiculo from "../src/vehiculo";
 import SistemaDriveHub from "../src/sistema";
-import {DeepMockProxy, mockDeep} from "jest-mock-extended";
+import { DeepMockProxy, mockDeep } from "jest-mock-extended";
+import IEstadoVehiculo from "../src/estadosVehiculo/IEstadoVehiculo";
 
 describe("Tests de la clase SistemaDriveHub", () => {
+  let sistema = new SistemaDriveHub();
 
-    let sistema = new SistemaDriveHub();
-    
-    afterEach(() => {
-        sistema["reservas"] = [];
-        sistema["mantenimientos"] = []
-    })
-    
-    it("Debe ser una instancia de la clase SistemaDriveHub", () => {
-        expect(sistema).toBeInstanceOf(SistemaDriveHub);
-    })
+  afterEach(() => {
+    sistema["reservas"] = [];
+    sistema["mantenimientos"] = [];
+  });
 
-    it("Debe poder crear una reserva si el vehiculo esta disponible", () => { 
-        const clienteMock : DeepMockProxy<Cliente> = mockDeep<Cliente>();
-        const vehiculoMock: DeepMockProxy<Vehiculo> = mockDeep<Vehiculo>();
-        vehiculoMock.estaDisponible.mockReturnValue(true);
+  it("Debe ser una instancia de la clase SistemaDriveHub", () => {
+    expect(sistema).toBeInstanceOf(SistemaDriveHub);
+  });
 
-        sistema.crearReserva(clienteMock, vehiculoMock, new Date(2025, 1, 1), new Date(2025, 1, 5));
-        expect(sistema["reservas"]).toHaveLength(1);      
-        }
-    )
+  it("Debe poder crear una reserva si el vehiculo esta disponible", () => {
+    const clienteMock = mockDeep<Cliente>();
+    const vehiculoMock = mockDeep<Vehiculo>();
 
-    it("No debe poder crear una reserva si el vehiculo no esta disponible", () => {
-        const clienteMock: DeepMockProxy<Cliente> = mockDeep<Cliente>();
-        const vehiculoMock: DeepMockProxy<Vehiculo> = mockDeep<Vehiculo>();
-        vehiculoMock.estaDisponible.mockReturnValue(false);
+      const estadoMock: IEstadoVehiculo = {
+        estaEnMantenimiento: jest.fn(),
+        puedeAlquilar: jest.fn().mockReturnValue(true),
+        asignarAlquiler: jest.fn(),
+        asignarDisponible: jest.fn(),
+        asignarMantenimiento: jest.fn(),
+        asignarLimpieza: jest.fn(),
+      };
 
-        expect(() => sistema.crearReserva(clienteMock, vehiculoMock, new Date(2025, 1, 1), new Date(2025, 1, 5))).toThrow("El vehiculo no esta disponible.")
-        expect(sistema["reservas"]).toHaveLength(0);
-        
-    })
+    vehiculoMock.getEstado.mockReturnValue(estadoMock);
 
+    sistema.crearReserva(
+      clienteMock,
+      vehiculoMock,
+      new Date(2025, 1, 1),
+      new Date(2025, 1, 5)
+    );
 
+    expect(sistema["reservas"]).toHaveLength(1);
+  });
 
+  it("No debe poder crear una reserva si el vehiculo no esta disponible", () => {
+    const clienteMock: DeepMockProxy<Cliente> = mockDeep<Cliente>();
+    const vehiculoMock: DeepMockProxy<Vehiculo> = mockDeep<Vehiculo>();
 
+      const estadoMock: IEstadoVehiculo = {
+        estaEnMantenimiento: jest.fn(),
+      puedeAlquilar: jest.fn().mockReturnValue(false),
+      asignarAlquiler: jest.fn(),
+      asignarDisponible: jest.fn(),
+      asignarMantenimiento: jest.fn(),
+      asignarLimpieza: jest.fn(),
+    };
 
-})
+    vehiculoMock.getEstado.mockReturnValue(estadoMock);
+
+    expect(() =>
+      sistema.crearReserva(
+        clienteMock,
+        vehiculoMock,
+        new Date(2025, 1, 1),
+        new Date(2025, 1, 5)
+      )
+    ).toThrow("El vehiculo no esta disponible.");
+    expect(sistema["reservas"]).toHaveLength(0);
+  });
+});
